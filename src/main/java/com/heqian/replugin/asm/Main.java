dac90ab6fe9531651ce9f94caded2e5235d3ec1e
heqian <182359041@qq.com>
2022-07-21 18:54:29 +0800

    完成

--- src/main/java/com/heqian/replugin/asm/Main.java
+++ src/main/java/com/heqian/replugin/asm/Main.java
@@ -10,9 +10,9 @@ public class Main {
             ApkFile apkFile = new ApkFile();
             apkFile.setZipalignPath("C:\\sdk\\Android\\build-tools\\33.0.0-rc2\\zipalign.exe");
             apkFile.setApksignerPath("C:\\sdk\\Android\\build-tools\\33.0.0-rc2\\apksigner.bat");
-            apkFile.setInZipFile(new File("C:\\Users\\Administrator\\Desktop\\1\\app.apk"));
-            apkFile.setOutZipFile(new File("C:\\Users\\Administrator\\Desktop\\1\\app-1.apk"));
-            apkFile.setKeyPath("D:\\dev\\RePlugin\\MyApplication\\key\\tilki_live.jks");
+            apkFile.setInZipFile(new File("D:\\dev\\live_app\\build\\app\\outputs\\flutter-apk\\app-release.apk"));
+            apkFile.setOutZipFile(new File("D:\\dev\\live_app\\build\\app\\outputs\\flutter-apk\\app-release1.apk"));
+            apkFile.setKeyPath("D:\\dev\\live_app\\android\\key\\tilki_live.jks");
             apkFile.setStorePassword("u2rXPYCf5wCYmrIW");
             apkFile.setKeyAlias("tilki");
             apkFile.setKeyPassword("3v4EHbA6pUTYh8PN");
@@ -20,8 +20,29 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
+
+
+//      public static void main(String[] args) {
+//        try {
+//            ApkFile apkFile = new ApkFile();
+//            apkFile.setZipalignPath("C:\\sdk\\Android\\build-tools\\33.0.0-rc2\\zipalign.exe");
+//            apkFile.setApksignerPath("C:\\sdk\\Android\\build-tools\\33.0.0-rc2\\apksigner.bat");
+//            apkFile.setInZipFile(new File("D:\\dev\\live_app\\build\\app\\outputs\\flutter-apk\\app-release.apk"));
+//            apkFile.setOutZipFile(new File("D:\\dev\\live_app\\build\\app\\outputs\\flutter-apk\\app-release1.apk"));
+//            apkFile.setKeyPath("D:\\dev\\live_app\\android\\key\\tilki_live.jks");
+//            apkFile.setStorePassword("u2rXPYCf5wCYmrIW");
+//            apkFile.setKeyAlias("tilki");
+//            apkFile.setKeyPassword("3v4EHbA6pUTYh8PN");
+//            apkFile.run();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//    }
+
+
+
 }
 
