7f04ead77df2dc91311d56687021a4ed0ca645d1
heqian <182359041@qq.com>
2022-08-24 16:46:28 +0800

    添加打包参数

--- src/main/java/com/heqian/replugin/asm/Main.java
+++ src/main/java/com/heqian/replugin/asm/Main.java
@@ -11,55 +11,14 @@ public class Main {
         try {
             parser = new CmdLineParser(apkFile);
             parser.parseArgument(args);
-            parser.printUsage(System.out);
+            apkFile.run();
         } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
             parser.printUsage(System.err);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-
 }
-
-//        try {
-//
-//
-//
-//            apkFile.setZipalignPath("C:\\sdk\\Android\\build-tools\\33.0.0-rc2\\zipalign.exe");
-//            apkFile.setApksignerPath("C:\\sdk\\Android\\build-tools\\33.0.0-rc2\\apksigner.bat");
-//            apkFile.setInZipFile(new File("D:\\dev\\live_app\\build\\app\\outputs\\flutter-apk\\app-release.apk"));
-//            apkFile.setOutZipFile(new File("D:\\dev\\live_app\\build\\app\\outputs\\flutter-apk\\app-release1.apk"));
-//            apkFile.setKeyPath("D:\\dev\\live_app\\android\\key\\tilki_live.jks");
-//            apkFile.setStorePassword("u2rXPYCf5wCYmrIW");
-//            apkFile.setKeyAlias("tilki");
-//            apkFile.setKeyPassword("3v4EHbA6pUTYh8PN");
-//            apkFile.run();
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
-
-//    public static void main(String[] args) {
-//        try {
-//            ApkFile apkFile = new ApkFile();
-//            apkFile.setZipalignPath("C:\\sdk\\Android\\build-tools\\33.0.0-rc2\\zipalign.exe");
-//            apkFile.setApksignerPath("C:\\sdk\\Android\\build-tools\\33.0.0-rc2\\apksigner.bat");
-//            apkFile.setInZipFile(new File("D:\\dev\\live_app\\build\\app\\outputs\\flutter-apk\\app-release.apk"));
-//            apkFile.setOutZipFile(new File("D:\\dev\\live_app\\build\\app\\outputs\\flutter-apk\\app-release1.apk"));
-//            apkFile.setKeyPath("D:\\dev\\live_app\\android\\key\\tilki_live.jks");
-//            apkFile.setStorePassword("u2rXPYCf5wCYmrIW");
-//            apkFile.setKeyAlias("tilki");
-//            apkFile.setKeyPassword("3v4EHbA6pUTYh8PN");
-//            apkFile.run();
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-
-
 
 
