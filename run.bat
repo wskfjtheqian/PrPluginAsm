f853f896df8d59e36c1f092e4107f0d16076edc6
heqian <182359041@qq.com>
2022-08-24 16:47:36 +0800

    添加打包参数

---
+++ run.bat
@@ -0,0 +1,9 @@
+java -jar .\RePluginAsm-1.0-SNAPSHOT.jar ^
+-z "C:\sdk\Android\build-tools\33.0.0-rc2\zipalign.exe" ^
+-g "C:\sdk\Android\build-tools\33.0.0-rc2\apksigner.bat" ^
+-i "D:\dev\live_app\build\app\outputs\flutter-apk\app-release.apk" ^
+-o "D:\dev\live_app\build\app\outputs\flutter-apk\app-release1.apk" ^
+-k "D:\dev\live_app\android\key\tilki_live.jks" ^
+-s "u2rXPYCf5wCYmrIW" ^
+-a "tilki" ^
+-p "3v4EHbA6pUTYh8PN"
